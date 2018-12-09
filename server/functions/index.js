4435b730ab3e8709010a97a15ea32df58a66bc55
silversteez <welcomematt@gmail.com>
2018-12-08 23:34:44 -0800

    Add more logging and wrap all reads in transaction to make sure its solid

--- server/functions/index.js
+++ server/functions/index.js
@@ -5,7 +5,7 @@ admin.initializeApp();
 
 exports.onGameUpdate = functions.firestore
   .document("games/{gameId}")
-  .onUpdate((change) => {
+  .onUpdate(change => {
     const gameData = change.after.data();
 
     // This is crucial to prevent infinite loops.
@@ -46,13 +46,13 @@ exports.onGameUpdate = functions.firestore
         hasControl = swapControlMap[hasControl];
         const controlTimeLimit = 40;
         const date = new Date();
-        enemyData.mana = round < 10 ? round: 10;
+        enemyData.mana = round < 10 ? round : 10;
         round++;
         phase = "preAttack";
         controlTimeOut = date.setSeconds(date.getSeconds() + controlTimeLimit);
         // Reset field state
-        playerData.field.forEach(card => card.willAttack = false);
-        enemyData.field.forEach(card => card.willAttack = false);
+        playerData.field.forEach(card => (card.willAttack = false));
+        enemyData.field.forEach(card => (card.willAttack = false));
       }
 
       // Update history
@@ -60,16 +60,23 @@ exports.onGameUpdate = functions.firestore
       history.push(update);
 
       // Check for game end
-      if (state !== GAME.complete && (playerData.life <= 0 || enemyData.life <= 0)) {
+      if (
+        state !== GAME.complete &&
+        (playerData.life <= 0 || enemyData.life <= 0)
+      ) {
         state = GAME.complete;
         playerData.didWin = playerData.life > 0;
         enemyData.didWin = enemyData.life > 0;
-        transactions.push(trs.update(db.collection("users").doc(playerData.id), {
+        transactions.push(
+          trs.update(db.collection("users").doc(playerData.id), {
             state: playerData.didWin ? "game_victory" : "game_loss"
-        }));
-        transactions.push(trs.update(db.collection("users").doc(enemyData.id), {
-          state: enemyData.didWin ? "game_victory" : "game_loss"
-        }));
+          })
+        );
+        transactions.push(
+          trs.update(db.collection("users").doc(enemyData.id), {
+            state: enemyData.didWin ? "game_victory" : "game_loss"
+          })
+        );
       }
 
       // Check for game end
@@ -77,25 +84,31 @@ exports.onGameUpdate = functions.firestore
         state = GAME.complete;
         playerData.didWin = false;
         enemyData.didWin = true;
-        transactions.push(trs.update(db.collection("users").doc(playerData.id), {
-          state: "game_loss"
-        }));
-        transactions.push(trs.update(db.collection("users").doc(enemyData.id), {
-          state: "game_victory"
-        }));
+        transactions.push(
+          trs.update(db.collection("users").doc(playerData.id), {
+            state: "game_loss"
+          })
+        );
+        transactions.push(
+          trs.update(db.collection("users").doc(enemyData.id), {
+            state: "game_victory"
+          })
+        );
       }
 
-      transactions.push(trs.update(change.after.ref, {
-        player1: playerKey === "player1" ? playerData : enemyData,
-        player2: playerKey === "player2" ? playerData : enemyData,
-        hasControl,
-        controlTimeOut,
-        history,
-        state,
-        round,
-        phase,
-        gameUpdateToCommit: null
-      }));
+      transactions.push(
+        trs.update(change.after.ref, {
+          player1: playerKey === "player1" ? playerData : enemyData,
+          player2: playerKey === "player2" ? playerData : enemyData,
+          hasControl,
+          controlTimeOut,
+          history,
+          state,
+          round,
+          phase,
+          gameUpdateToCommit: null
+        })
+      );
 
       return Promise.all(transactions);
     });
@@ -110,125 +123,136 @@ exports.updateUser = functions.firestore
 
     console.log(`User: ${userId}, state: ${user.state}`);
 
-    // Trying to catch weird bug
-    if (!userId || !user || !user.deck)  {
-      console.log('No userId? No user? No deck? WHAT?');
-      return null;
-    }
-
     // This is crucial to prevent infinite loops.
     if (user.state === prevUser.state) return null;
 
     // Only care about searching for now
-    if (user.state !== USER.searching && user.state !== USER.attempt_reconnect) return null;
+    if (user.state !== USER.searching && user.state !== USER.attempt_reconnect) {
+      return null;
+    }
 
     const db = admin.firestore();
     return db.runTransaction(async trs => {
-      const foundExistingGame = await trs
-        .get(db
-            .collection("games")
-            .where("users", "array-contains", userId)
-            .where("state", "==", GAME.active)
-            .limit(1))
-        .then(gameResult => {
-          if (gameResult.size === 1) {
-            const gameSnapshot = gameResult.docs[0];
-            const gameId = gameSnapshot.id;
-            //  add a reference to the game in the player document
-            trs.update(db.collection("users").doc(userId), {
-              gameId: gameId,
-              state: USER.found_game
-            });
-            console.log(`User: ${userId}, FOUND GAME!`);
-            return true;
-          }
-          return null;
-        });
-
-      if (foundExistingGame) {
+      // Now that we're in transaction, make SURE user is still searching for game...
+      const userRef = db.collection("users").doc(userId);
+      const userDoc = await trs.get(userRef);
+      if (!userDoc.exists) {
+        console.log(`User: ${userId}, no longer exists...`);
+        return null;
+      }
+      const state = userDoc.data().state;
+      if (state !== USER.searching && state !== USER.attempt_reconnect) {
+        console.log(`User: ${userId}, no longer searching or attempting reconnect...`);
         return null;
       }
 
+      let gameResult = await trs.get(
+        db
+          .collection("games")
+          .where("users", "array-contains", userId)
+          .where("state", "==", GAME.active)
+          .limit(1)
+      );
+
+      // Found game
+      if (gameResult.size === 1) {
+        const gameSnapshot = gameResult.docs[0];
+        const gameId = gameSnapshot.id;
+        console.log(`User: ${userId}, FOUND GAME!`);
+        //  add a reference to the game in the player document
+        return trs.update(userRef, {
+          gameId: gameId,
+          state: USER.found_game
+        });
+      }
+
       // If we're attempting reconnect on app load and we didn't find a game, enter menu state
-      if (!foundExistingGame && user.state === "attempt_reconnect") {
-        console.log(`User: ${userId}, No game to reconnect to, enter menu state!`);
-        return trs.update(db.collection("users").doc(userId), {
+      if (!gameResult.size && user.state === "attempt_reconnect") {
+        console.log(
+          `User: ${userId}, No game to reconnect to, enter menu state!`
+        );
+        return trs.update(userRef, {
           state: USER.menu
         });
       }
 
-      return trs
-        .get(
-          db
-            .collection("games")
-            .where("full", "==", false)
-            .limit(1)
-        )
-        .then(gameResult => {
-          let gameId;
-          if (gameResult.size === 1) {
-            // a game was found, add the player to it
-            const gameSnapshot = gameResult.docs[0];
-            const game = gameSnapshot.data();
-            const users = game.users.concat([userId]);
-            const full = users.length === 2;
-            const player2 = Object.assign(game.player2, {id: userId, deck: user.deck});
-            if (userId === null || user.deck === null) {
-              console.log('BUG null issue', user);
-            }
-            const controlTimeLimit = 40;
-            const date = new Date();
-            const newGameData = {
-              full,
-              users,
-              player2,
-              controlTimeLimit,
-              controlTimeOut: date.setSeconds(date.getSeconds() + controlTimeLimit),
-              state: GAME.active
-            };
-            trs.update(gameSnapshot.ref, newGameData);
-            gameId = gameSnapshot.id;
-            console.log(`User: ${userId}, joined game`);
-          } else {
-            // no game was found, create a new game with the player
-            const users = [userId];
-            const gameRef = db.collection("games").doc();
-            trs.set(gameRef, {
-              full: false,
-              users: users,
-              hasControl: "player1",
-              round: 0,
-              phase: "preAttack",
-              controlTimeLimit: null,
-              controlTimeOut: null,
-              player1: {
-                id: userId,
-                life: 30,
-                mana: 1,
-                deck: user.deck,
-                hand: [],
-                field: []
-              },
-              player2: {
-                id: null,
-                life: 30,
-                mana: 1,
-                deck: null,
-                hand: [],
-                field: []
-              },
-              history: [],
-              state: GAME.active,
-              gameUpdateToCommit: null
-            });
-            gameId = gameRef.id;
-            console.log(`User: ${userId}, created game`);
-          }
-          // then add a reference to the game in the player document
-          return trs.update(db.collection("users").doc(userId), {
-            gameId: gameId,
-            state: USER.found_game
-          });
+      // Find a game that needs player2
+      gameResult = await trs.get(
+        db
+          .collection("games")
+          .where("full", "==", false)
+          .limit(1)
+      );
+
+      let gameId;
+      let transactions = [];
+      if (gameResult.size === 1) {
+        // A game was found, add the player to it
+        const gameSnapshot = gameResult.docs[0];
+        const game = gameSnapshot.data();
+        const users = game.users.concat([userId]);
+        const full = users.length === 2;
+        const player2 = Object.assign(game.player2, {
+          id: userId,
+          deck: user.deck
         });
+        const controlTimeLimit = 40;
+        const date = new Date();
+        const newGameData = {
+          full,
+          users,
+          player2,
+          controlTimeLimit,
+          controlTimeOut: date.setSeconds(date.getSeconds() + controlTimeLimit),
+          state: GAME.active
+        };
+        transactions.push(trs.update(gameSnapshot.ref, newGameData));
+        gameId = gameSnapshot.id;
+        console.log(`User: ${userId}, joined game`);
+      } else {
+        // No game was found, create a new game with the player
+        const users = [userId];
+        const gameRef = db.collection("games").doc();
+        transactions.push(
+          trs.set(gameRef, {
+            full: false,
+            users: users,
+            hasControl: "player1",
+            round: 0,
+            phase: "preAttack",
+            controlTimeLimit: null,
+            controlTimeOut: null,
+            player1: {
+              id: userId,
+              life: 30,
+              mana: 1,
+              deck: user.deck,
+              hand: [],
+              field: []
+            },
+            player2: {
+              id: null,
+              life: 30,
+              mana: 1,
+              deck: null,
+              hand: [],
+              field: []
+            },
+            history: [],
+            state: GAME.active,
+            gameUpdateToCommit: null
+          })
+        );
+        gameId = gameRef.id;
+        console.log(`User: ${userId}, created game`);
+      }
+      // then add a reference to the game in the player document
+      transactions.push(
+        trs.update(userRef, {
+          gameId: gameId,
+          state: USER.found_game
+        })
+      );
+      return Promise.all(transactions);
     });
   });
